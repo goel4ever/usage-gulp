426a1d8b09d677ca62bd92196aa1c7e157ad769c
Anshul Goel <anshul.goel@priceline.com>
2015-05-05 20:07:10 -0400

    Added first gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/**
+ * Created by agoel on 5/4/15.
+ */
+
+'use strict';
+
+// require - Node method that tells that gulp is required by the code
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
+
+var jsSources = [
+  'components/**/*.js'
+];
+var jsDestination = 'builds/development/js';
+
+var sassSources = [
+  'components/**/*.scss'
+];
+var sassDestination = 'builds/development/css';
+
+gulp.task('default', function() {
+  gutil.log('Learning Gulp')
+    .on('error', gutil.log);
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js')
+      .on('error', gutil.log))
+    .pipe(gulp.dest(jsDestination));
+});
+
+gulp.task('', function() {
+  gulp.src(sassSources)
+    .pipe(compass())
+    .gulp.dest(sassDestination);
+});
